ec1d94044083d07dcdf1a9a5a67450405907e668
rsax11 <59619988+rsax11@users.noreply.github.com>
2020-01-14 13:00:07 -0800

    Add files via upload

---
+++ LuckySpin/LuckySpin/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace LuckySpin
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<Lucky7>();
+        }
+
+       public void Configure(IApplicationBuilder app, IHostingEnvironment env, Lucky7 Lucky7)
+        {
+            app.Run(async (context) =>
+            {
+            await context.Response.WriteAsync(Lucky7.Output());
+            });
+
+        }
+    }
+}
